215006cd0c0704b2f30e6955d6a49ae45ef6b920
qetmes <55390882+qetmes@users.noreply.github.com>
2021-03-09 21:45:38 +0800

    maven web工程

---
+++ 002-link-consumer/src/main/java/com/guo/dubbo/web/UserController.java
@@ -0,0 +1,23 @@
+package com.guo.dubbo.web;
+
+
+import com.guo.dubbo.model.User;
+import com.guo.dubbo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "/user")
+    public String userDetail(Model model, Integer id){
+        User user = userService.queryUserById(id);
+        model.addAttribute("user",user);
+        return "userDetail";
+    }
+}
---
+++ 002-link-consumer/src/main/webapp/userDetail.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: GUO
+  Date: 2021/3/9
+  Time: 15:54
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>用户详情</title>
+</head>
+<body>
+<h1>用户详情</h1>
+<div>用户标识：${user.id}</div>
+<div>用户名称：${user.username}</div>
+
+</body>
+</html>
