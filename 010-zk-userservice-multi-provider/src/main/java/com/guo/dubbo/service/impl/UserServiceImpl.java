913c36d76d3870719777e5573f4aeed2492bec48
qetmes <55390882+qetmes@users.noreply.github.com>
2021-03-10 11:25:23 +0800

    多接口实现类下的提供者工程

---
+++ 010-zk-userservice-multi-provider/src/main/java/com/guo/dubbo/service/impl/UserServiceImpl.java
@@ -0,0 +1,16 @@
+package com.guo.dubbo.service.impl;
+
+import com.guo.dubbo.model.User;
+import com.guo.dubbo.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public User queryUserById(Integer id, String username) {
+        User user = new User();
+        user.setId(id);
+        user.setUsername(username+"-1");
+
+        return user;
+    }
+}
